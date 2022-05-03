3326da874490e68eca2bedd17c393b5023160157
Moist-Cat <moistanonpy@gmail.com>
2022-05-03 19:42:43 -0400

    Modified the pause/continue behaviour.

--- crawl.py
+++ crawl.py
@@ -1,28 +1,77 @@
+import time
 import os
-from collections import deque
-#from urlparse import ...
 from urllib.parse import urlparse,urljoin
 
+import requests
 from requests import Session
 from bs4 import BeautifulSoup as bs
 
+def check_errors(request):
+    def inner_func(cls, method, url, **kwargs):
+        request_success = False
+        while not request_success:
+            try:
+                start = time.time()
+                print(f'Trying to {method} data from {url}...')
+                response = request(cls, method, url, **kwargs)
+                time_elapsed = time.time() - start
+                print(
+                    f'The response from the server after {time_elapsed:.2f} seconds '\
+                    f'was {len(response.text)} characters big. URL: {response.url}'
+                )
+                response.raise_for_status()
+            except (
+                    requests.exceptions.ConnectionError, requests.exceptions.SSLError
+            ):
+                print('Something went wrong. Retrying...')
+                print(
+                    f'Server URL: {url}), failed while trying to connect.'
+                )
+            except requests.exceptions.HTTPError:
+                raw_response = response.content[:20] if len(response.content) > 50 else response.content
+                try:
+                    payload = kwargs['data']
+                except KeyError:
+                    payload = 'none'
+                error_message = f'Server URL: {response.url}, ' \
+                    f'failed with status code ({response.status_code}). ' \
+                    f'Raw response: {raw_response}' \
+                    f'Request payload: {payload}'
+
+                print(error_message)
+                if response.status_code < 499:
+                    # return if client error
+                    request_success = True
+            else:
+                request_success = True
+            time.sleep(0.2)
+        return response
+    return inner_func
+
+class Session(Session):
+    """
+    Overrriden version of requests.Session that checks for errors
+    after completing the request.
+    """
+
+    @check_errors
+    def request(self, method, url, **kwargs):
+        return super().request(method, url, **kwargs)
+
 session = Session()
 
-LINKS_FILE = 'dumps.txt'
-
-def load_links(path=LINKS_FILE):
+def load_links(path):
     with open(path) as file:
         data = file.read()
     return data.split("\n")
 
-def dump_link(link, path=LINKS_FILE):
+def dump_link(url, path):
     with open(path, 'a') as file:
-        file.write(link + "\n")
+        file.write(url + "\n")
 
 def get_file(url, remote_file):
     file_name = url.split('/')[-1]
     o = urlparse(url)
-#    temp = o.path
     temp = o.path
     temp = location + temp
     ind = temp.find(file_name)
@@ -36,23 +85,24 @@ def get_file(url, remote_file):
 
 def link_collect(url):
     if url in visited:
-        #Visited link removed from list to prevent infinite loopi
         link.remove(url)
         return -1
-    visited.append(url)
-#    response = urllib2.urlopen(url)
+
     response = session.get(url)
-#    html_doc = response.read()
     html_doc = response.text
-#    get_file(url,html_doc)
     get_file(url, html_doc)
+
+    visited.add(url)
+    dump_link(url, path="visited.txt")
+
     #If link returns an HTML page then parse using BeautifulSoup
     if url.find('.html') + 1:
         soup = bs(html_doc)
     else:
         return
-    for hRef in soup.find_all('a'):
-        print(hRef)
+
+    hRefs = set(soup.find_all('a'))
+    for hRef in hRefs:
         try:
             #If link doesn't have href attribute then continue
             hRef['href']
@@ -67,44 +117,52 @@ def link_collect(url):
                	#Used to get details about the link such as the host name, url, etc.
                 o = urlparse(urljoin(url,temp))
                 if o.netloc == 'docs.python.org':
-                    url = o.geturl()
-                    if url not in visited:
-                        link.append(o.geturl())
-                        dump_link(url)
-            ext_files = soup.find_all('link') + soup.find_all('script')
-            for j in ext_files:
-                try:
-                    temp = j['src']
-                except KeyError:
-                    try:
-                        temp = j['href']
-                    except KeyError:
-                        continue
-                temp1 = urljoin(url,temp)
-                if temp1 in visited:
-                    continue
-                else:
-                    link.append(temp1)
-                    dump_link(temp1)
+                    u = o.geturl()
+                    if u not in visited:
+                        print(f"Added {u} to the queue")
+                        if len(link) < 10000:
+                            link.add(u)
+                            dump_link(u, "dumps.txt")
+                        else:
+                            link_collect(u)
 
-
+    ext_files = soup.find_all('link') + soup.find_all('script')
+    for j in set(ext_files):
+        try:
+            temp = j['src']
+        except KeyError:
+            try:
+                temp = j['href']
+            except KeyError:
+                continue
+        temp1 = urljoin(url, temp)
+        if temp1 in visited:
+            continue
+        if len(link) > 10000:
+            link_collect(temp1)
+        else:
+            link.add(temp1)
+            dump_link(temp1, "dumps.txt")
 
 if __name__ == "__main__":
     location = '/var/www/pydocs/'
-    link = deque()
-    old_links = load_links()
-    if any(old_links):
-        link.extend(old_links)
+
+    link = set() 
+    links = load_links("dumps.txt")
+    if any(links):
+        link.update(links)
     else:
-        link.append('http://docs.python.org/tutorial/datastructures.html')
-    visited = []
-    while (link):
-        print(link[0])
-        i = link_collect(link[0])
+        link.add('http://docs.python.org/tutorial/datastructures.html')
+
+    visited = set()
+    already_visited = load_links(path="visited.txt")
+    if any(already_visited):
+        visited.update(already_visited)
+    while True:
         try:
-            link.popleft()
+            l = next(iter(link))
         except IndexError:
             break
-        else:
-            if i!=-1:
-                print(len(visited))
+        i = link_collect(l)
+        if i != -1:
+            print(f"{len(visited)} pages been mirrored already.")
